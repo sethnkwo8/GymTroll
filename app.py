bf9b7c9dad9457438e327b06c6c0b27d6fe22afc
Seth Nkwo <sethnkwocool@gmail.com>
2025-04-16 00:59:47 +0100

    Adjusted carousel size and added images for all 5 slides. Added Log in and Register pages and buttons and Get Started Button and initial footer

--- app.py
+++ app.py
@@ -13,3 +13,11 @@ Session(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
