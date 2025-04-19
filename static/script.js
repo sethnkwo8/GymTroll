a054d4cac9e48c1fd386fa1a09178b66330cec50
Seth Nkwo <sethnkwocool@gmail.com>
2025-04-19 22:12:37 +0100

    Update script.js
    Modified navbar, cards and navbar items

--- static/script.js
+++ static/script.js
@@ -1,11 +1,17 @@
-// Hide navbar when you scroll down
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-50px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+
+    if (currentScrollPos === 0) {
+        // Always show the navbar when at the top of the page
+        document.getElementById("navbar").style.top = "0";
+    } else if (prevScrollpos > currentScrollPos) {
+        // Show the navbar when scrolling up
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        // Hide the navbar when scrolling down
+        document.getElementById("navbar").style.top = "-100px"; // Adjust height if needed
+    }
+
+    prevScrollpos = currentScrollPos;
+};
